3dfe6b07b82718fb0873d2ef8f2ddbfc3e425555
briefl <artembrifl@gmail.com>
2025-04-25 17:57:53 +0300

    build files

---
+++ CMakeFiles/my-project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my-project.dir/src/archiver.cpp.o"
+  "CMakeFiles/my-project.dir/src/archiver.cpp.o.d"
+  "CMakeFiles/my-project.dir/src/main.cpp.o"
+  "CMakeFiles/my-project.dir/src/main.cpp.o.d"
+  "my-project"
+  "my-project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
